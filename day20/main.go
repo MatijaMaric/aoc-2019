670497e90e9cfc581642ecedd2e7005cc4da91c7
Matija Marić <sirSlani@gmail.com>
2019-12-26 17:51:54 +0100

    ja ne znam, pusti me da umrem

--- day20/main.go
+++ day20/main.go
@@ -2,173 +2,170 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"sort"
 	"strings"
 
 	"github.com/MatijaMaric/aoc-2019/utils"
 )
 
+var (
+	up    = vec2{Y: -1}
+	right = vec2{X: 1}
+	down  = vec2{Y: 1}
+	left  = vec2{X: -1}
+)
+
+var directions = []vec2{up, right, down, left}
+
 type vec2 = utils.Vector2D
 
-type portal struct {
-	name     string
-	from, to vec2
+type node struct {
+	pos    vec2
+	to     []vec2
+	offset int
 }
 
 func main() {
-	grid := utils.ReadGrid("input.txt")
-	portals := findPortals(grid)
-	edges := findEdges(grid, portals)
-
-	dists := findPath(edges)
-
-	fmt.Println(dists["ZZ"] - 1)
-}
-
-func findPath(edges map[string](map[string]int)) map[string]int {
-	dists := make(map[string]int)
-	var q []string
-	for k := range edges {
-		dists[k] = math.MaxInt32
-		q = append(q, k)
-	}
-
-	dists["AA"] = 0
-
-	for len(q) > 0 {
-		sort.Slice(q, func(i, j int) bool {
-			return dists[q[i]] < dists[q[j]]
-		})
-
-		current := q[0]
-		q = q[1:]
-
-		for k := range edges[current] {
-			dist := edges[current][k] + dists[current]
-			if dist < dists[k] {
-				dists[k] = dist
-			}
-		}
-	}
-
-	return dists
-}
-
-var (
-	up    = vec2{Y: -1}
-	down  = vec2{Y: 1}
-	left  = vec2{X: -1}
-	right = vec2{X: 1}
-)
-
-var directions = []vec2{up, down, left, right}
-
-func findEdgesRec(grid map[vec2]rune, portals map[string]*portal, from string) map[string](map[string]int) {
-	ans := make(map[string](map[string]int))
-
-	// edges := bfs(grid, portals[from].from)
-
-	return ans
-}
-
-func findEdges(grid map[vec2]rune, portals map[string]*portal) map[string](map[string]int) {
-	ans := make(map[string](map[string]int))
-
-	for k, from := range portals {
-		edges := bfs(grid, from.from)
-		edgesTo := bfs(grid, from.to)
-		for kt, vt := range edgesTo {
-			if vf, ok := edges[kt]; ok {
-				if vt < vf {
-					edges[kt] = vt
-				}
-			} else {
-				edges[kt] = vt
-			}
-		}
-		ans[k] = edges
-	}
-
-	return ans
-}
-
-type queue struct {
-	pos  vec2
-	dist int
-	path []vec2
-}
-
-func bfs(grid map[vec2]rune, from vec2) map[string]int {
-	q := []queue{queue{from, 1, []vec2{from}}}
-	visited := make(map[vec2]bool)
-	ans := make(map[string]int)
-
-	for len(q) > 0 {
-		current := q[0]
-		q = q[1:]
-
-		visited[current.pos] = true
-		for _, dir := range directions {
-			next := current.pos.Add(dir)
-			if !visited[next] {
-				if grid[next] == '.' {
-					q = append(q, queue{next, current.dist + 1, append(current.path, next)})
-				}
-				if isLetter(grid[next]) && current.dist != 1 {
-					p, _ := getPortal(grid, next)
-					ans[p.name] = current.dist
-				}
-			}
-		}
-	}
-
-	return ans
-}
-
-func findPortals(grid map[vec2]rune) map[string]*portal {
-	ans := make(map[string]*portal)
+	grid := utils.ReadGrid("test.txt")
 	dim := utils.GridSize(grid)
+	nodes, start, end := getNodes(grid)
 	for y := 0; y < dim.Y; y++ {
 		for x := 0; x < dim.X; x++ {
-			pos := vec2{X: x, Y: y}
-			val := grid[pos]
-			if isLetter(val) {
-				if p, ok := getPortal(grid, pos); ok {
-					if _, ok := ans[p.name]; ok {
-						ans[p.name].to = p.from
-					} else {
-						ans[p.name] = &portal{p.name, p.from, vec2{}}
-					}
+			if v, ok := nodes[vec2{X: x, Y: y}]; ok && v.offset != 0 {
+				if v.offset > 0 {
+					fmt.Print("^")
+				} else {
+					fmt.Print("v")
 				}
+			} else {
+				fmt.Printf("%c", grid[vec2{X: x, Y: y}])
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println(bfs(nodes, start, end, false, grid))
+	fmt.Println(bfs(nodes, start, end, true, grid))
+}
+
+type position struct {
+	vec   vec2
+	level int
+}
+
+func bfs(nodes map[vec2]*node, start, end vec2, recursive bool, grid map[vec2]rune) int {
+	type item struct {
+		pos  position
+		dist int
+		path []position
+	}
+	q := []item{item{position{start, 0}, 0, []position{position{start, 0}}}}
+	visited := make(map[position]bool)
+	visited[position{start, 0}] = true
+
+	for len(q) > 0 {
+		current := q[0]
+		q = q[1:]
+		printGrid(grid, current.path)
+		if current.pos.vec.Eq(end) && current.pos.level == 0 {
+			return current.dist
+		}
+
+		// fmt.Println(current)
+		for _, next := range nodes[current.pos.vec].to {
+			level := current.pos.level
+			if recursive {
+				level += nodes[current.pos.vec].offset
+			}
+			nextPos := position{next, level}
+			dist := current.dist + 1
+			if level >= 0 && !visited[nextPos] {
+				visited[nextPos] = true
+				q = append(q, item{nextPos, dist, append(current.path, nextPos)})
 			}
 		}
 	}
-	return ans
+
+	panic("noooo")
+}
+
+func getNodes(grid map[vec2]rune) (nodes map[vec2]*node, start, end vec2) {
+	nodes = make(map[vec2]*node)
+	portals := make(map[string]([]vec2))
+	for pos, c := range grid {
+		if c == '.' {
+			nodes[pos] = &node{pos, []vec2{}, 0}
+			for _, dir := range directions {
+				neighbor := pos.Add(dir)
+				if grid[neighbor] == '.' {
+					nodes[pos].to = append(nodes[pos].to, neighbor)
+				}
+			}
+		}
+		if n, p := grid[pos.Add(up)], grid[pos.Add(down)]; isLetter(c) && (isLetter(n) || isLetter(p)) {
+			if n == '.' {
+				label := fmt.Sprintf("%c%c", c, p)
+				portals[label] = append(portals[label], pos.Add(up))
+			}
+			if p == '.' {
+				label := fmt.Sprintf("%c%c", n, c)
+				portals[label] = append(portals[label], pos.Add(down))
+			}
+		}
+		if n, p := grid[pos.Add(left)], grid[pos.Add(right)]; isLetter(c) && (isLetter(n) || isLetter(p)) {
+			if n == '.' {
+				label := fmt.Sprintf("%c%c", c, p)
+				portals[label] = append(portals[label], pos.Add(left))
+			}
+			if p == '.' {
+				label := fmt.Sprintf("%c%c", n, c)
+				portals[label] = append(portals[label], pos.Add(right))
+			}
+		}
+	}
+	// fmt.Println(portals)
+	dim := utils.GridSize(grid)
+	for label, pos := range portals {
+		if strings.Compare(label, "AA") == 0 {
+			start = pos[0]
+			continue
+		} else if strings.Compare(label, "ZZ") == 0 {
+			end = pos[0]
+			continue
+		}
+		if pos[0].X == 2 || pos[0].Y == 2 || pos[0].X == dim.X-3 || pos[0].Y == dim.Y-3 {
+			nodes[pos[0]].offset = -1
+		} else {
+			nodes[pos[0]].offset = 1
+		}
+		nodes[pos[0]].to = append(nodes[pos[0]].to, pos[1])
+		if pos[1].X == 2 || pos[1].Y == 2 || pos[1].X == dim.X-3 || pos[1].Y == dim.Y-3 {
+			nodes[pos[1]].offset = -1
+		} else {
+			nodes[pos[1]].offset = 1
+		}
+		nodes[pos[1]].to = append(nodes[pos[1]].to, pos[0])
+	}
+	return
 }
 
 func isLetter(c rune) bool {
 	return c >= 'A' && c <= 'Z'
 }
 
-func getPortal(grid map[vec2]rune, pos vec2) (portal, bool) {
-	var builder strings.Builder
-	var entry vec2
-	for y := pos.Y - 1; y <= pos.Y+1; y++ {
-		for x := pos.X - 1; x <= pos.X+1; x++ {
-			p := vec2{X: x, Y: y}
-			c := grid[p]
-			if isLetter(c) {
-				builder.WriteRune(c)
-			}
-			if c == '.' {
-				entry = p
+func printGrid(grid map[vec2]rune, path []position) {
+	visited := make(map[vec2]rune)
+	for _, v := range path {
+		visited[v.vec] = rune(v.level) + '1'
+	}
+	dim := utils.GridSize(grid)
+	for y := 0; y < dim.Y; y++ {
+		for x := 0; x < dim.X; x++ {
+			if v, ok := visited[vec2{X: x, Y: y}]; ok {
+				fmt.Printf("%c", v)
+			} else {
+				fmt.Printf("%c", grid[vec2{X: x, Y: y}])
 			}
 		}
+		fmt.Println()
 	}
-	ans := builder.String()
-	if len(ans) == 2 && !entry.Eq(vec2{}) {
-		return portal{name: ans, from: entry}, true
-	}
-	return portal{}, false
+	fmt.Println()
 }
